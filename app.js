d5a1a283d5925dd3bf8003203e51a5b895965682
Narbir Lamba <152689807+narbirlamba@users.noreply.github.com>
2024-02-01 23:53:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5,3 +5,35 @@ function toggleMenu() {
     user.style.color = (user.style.color === 'black') ? 'white' : 'black';
     
 }
+
+const media =()=>{
+    let q = matchMedia("(max-width:700px)");
+    if(q.matches){
+        let scrollContainer1 = document.querySelector(".gallery");
+        let backBtn1 = document.getElementById("backbtn");
+        let nextBtn1 = document.getElementById("nextbtn");
+
+    nextBtn1.addEventListener("click",()=>{
+      scrollContainer1.style.scrollBehavior = "smooth";
+      scrollContainer1.scrollLeft += 143;
+    })
+    backBtn1.addEventListener("click",()=>{
+      scrollContainer1.style.scrollBehavior = "smooth";
+      scrollContainer1.scrollLeft -= 143;
+    })
+    }else{
+        let scrollContainer1 = document.querySelector(".gallery");
+        let backBtn1 = document.getElementById("backbtn");
+        let nextBtn1 = document.getElementById("nextbtn");
+
+    nextBtn1.addEventListener("click",()=>{
+      scrollContainer1.style.scrollBehavior = "smooth";
+      scrollContainer1.scrollLeft += 220;
+    })
+    backBtn1.addEventListener("click",()=>{
+      scrollContainer1.style.scrollBehavior = "smooth";
+      scrollContainer1.scrollLeft -= 220;
+    })
+    }
+}
+onload = media;
